f8cbde41eae78ff349ce951583f1f2b21481728c
Test <Test>
2020-12-18 11:37:40 +0300

    Initial Commit

---
+++ PanRacing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PanRacing
+//
+//  Created by user on 18/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
